f3371353aa41b36fe930fbd6fca48fed5b026857
Indrek Soidla <indrek.soidla@ut.ee>
2022-09-09 09:41:20 +0300

    Initial commit

---
+++ ka_2022_1_sissejuhatus_praktikum_lahendused.R
@@ -0,0 +1,13 @@
+# Kvantitatiivsete meetodite aluskursus
+# Praktikum 1: töö andmestikuga
+
+# Siia skriptifaili saate kirjutada ülesannete lahendamiseks vajaliku koodi. Hea on koodi lisada ka selgitavaid kommentaare, et hiljem oleks endal lihtsam koodis orienteeruda ja aru saada, miks olete mingi käsu kirjutanud ja mis eesmärgil teinud analüüsis mingeid valikuid. Inimese mälu ei mahuta palju infot ja kunagi hiljem võib ilma selgitusteta olla keeruline aru saada, miks teie mineviku mina on mingeid käske just sellisel viisil kokku kirjutanud nagu nad failis on.
+
+# Laeme andmed sisse
+
+pgmd <- read.csv2("data/pgmdv2.1_group_is.csv")
+
+# Skriptifaili saab jagada alaosadeks, lisades kommentaari taha neli trelli (tasakaalu mõttes võib lisaks ka ette :)).
+
+#### Ülesanne 1.1 ####
+
